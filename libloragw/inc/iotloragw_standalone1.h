82445da9f74ca9acc25fa9fdf6c3be4b0c9718c1
Ryan Walmsley <ryanteck@gmail.com>
2019-10-01 17:20:33 +0100

    And SPI 1

---
+++ libloragw/inc/iotloragw_standalone1.h
@@ -0,0 +1,23 @@
+/*
+ * iotloragw_rpi.h
+ *
+ *  Created on: Aug 09, 2018
+ *      Author: ryanteck
+ */
+
+#ifndef _IOTGW_RPI_H_
+#define _IOTGW_RPI_H_
+
+/* Human readable platform definition */
+#define DISPLAY_PLATFORM "IOTLGW RPi"
+
+/* parameters for native spi */
+#define SPI_SPEED		5000000
+#define SPI_DEV_PATH	"/dev/spidev2.1"
+#define SPI_CS_CHANGE   0
+
+/* parameters for a FT2232H */
+#define VID		        0x0403
+#define PID		        0x6014
+
+#endif /* _IOTGW_RPI_H_ */
